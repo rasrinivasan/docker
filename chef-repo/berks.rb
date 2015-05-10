0db20da3b51ef6558eb7094c733dca911b1c1f99
rasrinivasan <netlimit2000@gmail.com>
2015-05-10 07:07:48 -0700

    Update berks.rb

--- chef-repo/berks.rb
+++ chef-repo/berks.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
+puts "About to subshell"
 Dir.chdir("/var/demo/apps/chef-repo") do
    system "berks install"
    system "berks vendor cookbooks/"
 end
+puts "subshell task completed. Look for errors."
 exit 0
